44e7cfd931eb055f8e3f93b9a0f3c50769b9c150
sharo <mohamedsamy9029@gmail.com>
2026-06-23 16:59:34 +0300

    update

--- full-version/routes/web.php
+++ full-version/routes/web.php
@@ -3,10 +3,7 @@
 use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Route;
 
-// Redirect root to frontend home
-Route::get('/', function () {
-    return redirect()->route('home');
-});
+
 
 // Dashboard home (protected by auth:admin middleware in admin.php)
 Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard.home');
